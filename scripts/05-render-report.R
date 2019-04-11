106f096bcbe87275d08e30fc06a0df9b64e4bc14
Tobin Magle <cmtobin@wisc.edu>
2019-04-11 16:07:11 -0500

    edited script to output the plot reports in a reports folder

--- scripts/05-render-report.R
+++ scripts/05-render-report.R
@@ -1,24 +1,26 @@
 library(rmarkdown)
 
+#Render a markdown document
+render(input = "scripts/05-reports-shiny.Rmd")
+
 #Render a markdown document with a specified parameter
-render(input = "Markdown_demo.Rmd", 
-       output_format = "html_document",
-       params = list( file = "data/surveys_complete.csv",
-                 sex = "F"))
+render(input = "scripts/05-reports-shiny.Rmd", 
+       params = list( file = "../data/complete_surveys.csv",
+                      sex = "F", 
+                      weight = 40))
 
 #Render a markdown document with a UI to select parameters
-render(input = "Markdown_demo.Rmd", 
-       output_format = "html_document",
+render(input = "scripts/05-reports-shiny.Rmd", 
        params = "ask")
 
 #Render several markdown documents with different data files as input
 
 #get a list of files
-files<-list.files(path = "data", pattern=".csv")
+files<-list.files(path = "data/by_plot", 
+                  pattern=".csv")
 
 #loop through the files, render a doc for each one.
 for (f in files){
-   render(input = "Markdown_demo.Rmd", 
-          output_format = "html_document",
-          output_file = paste(f, ".html", sep = ""))
+   render(input = "scripts/05-reports-shiny.Rmd",
+          output_file = paste("../reports/",f, ".html", sep = ""))
 }
